3a2f355517e3dc4c664a79359531d79a381dfd25
Eric Grudzien <egru@amazon.com>
2024-12-18 07:34:28 -0800

    add comment 3, and see if kicks off a pipeline.

--- pipelines/run_pipeline.py
+++ pipelines/run_pipeline.py
@@ -20,7 +20,7 @@ import traceback
 
 from pipelines._utils import get_pipeline_driver, convert_struct, get_pipeline_custom_tags
 
-# EG: Comment 2
+# EG: Comment 3
 
 def main():  # pragma: no cover
     """The main harness that creates or updates and runs the pipeline.
